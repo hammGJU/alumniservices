f5fb1d4326799c4ceb034614b7f8f2c985acd280
hbata <hesham.bataineh@aiesec.net>
2017-11-27 05:37:59 +0200

    Implement simple login bean for testing

---
+++ src/main/java/beans/LoginBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author hesham
+ */
+@SessionScoped
+@Named(value = "loginBean")
+public class LoginBean implements Serializable {
+
+    private String username;
+    private String password;
+    private Connection conn;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void login() {
+        FacesContext facesContect = FacesContext.getCurrentInstance();
+        boolean success = true;
+        if (success) {
+            if (facesContect != null) {
+                NavigationHandler navigationHandler = facesContect.getApplication().getNavigationHandler();
+                navigationHandler.handleNavigation(facesContect, null, "/first_page?faces-redirect=true");
+            }
+        }
+    }
+
+}
